c42af5898a94ef622695502abc04b164d28a3972
Harish78100 <110106936+Harish78100@users.noreply.github.com>
2024-01-11 15:39:04 +0530

    User authentication controller added

---
+++ Controllers/UserAuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureADClientSideAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserAuthenticationController : ControllerBase
+    {
+        [HttpGet("user-authetication")]
+        public IActionResult SampleAction()
+        {
+            string? username = User.Identity.Name; 
+            string message = $"Hello, {username}! This is a protected resource.";
+            return Ok(new { Message = message });
+        }
+
+    }
+}
